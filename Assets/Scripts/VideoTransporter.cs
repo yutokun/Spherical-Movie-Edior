1ff474382a606d0320e4673de40a8e6bd810372d
yutoVR <yuto.fushimi@icloud.com>
2020-06-08 00:25:45 +0900

    First commit.

---
+++ Assets/Scripts/VideoTransporter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoTransporter : MonoBehaviour
+{
+    [SerializeField]
+    Material mat;
+
+    VideoPlayer video;
+
+    void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        mat.mainTexture = video.texture;
+    }
+}
