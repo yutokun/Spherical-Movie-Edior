8d72275412efbae09aff2f3e3c2f346588e70d48
yutoVR <yuto.fushimi@icloud.com>
2020-06-08 00:39:32 +0900

    ビデオ時間で Timeline を再生

---
+++ Assets/Scripts/TimelinePlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class TimelinePlayer : MonoBehaviour
+{
+    [SerializeField]
+    VideoPlayer video;
+
+    PlayableDirector director;
+
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    void Update()
+    {
+        director.time = video.time;
+        director.Evaluate();
+    }
+}
