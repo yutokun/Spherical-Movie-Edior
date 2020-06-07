f35d220425c9dbd5cf3cf7fcf19ea9c851def5dd
yutoVR <yuto.fushimi@icloud.com>
2020-06-08 02:58:23 +0900

    wip 1フレームずつ記録

---
+++ Assets/Scripts/VideoFrameGetter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoFrameGetter : MonoBehaviour
+{
+    [SerializeField]
+    VideoPlayer video;
+
+    long frame = 1, frameCount;
+    bool isPlayable;
+
+    void Start()
+    {
+        frameCount = (long)video.frameCount;
+        video.sendFrameReadyEvents = true;
+        video.started += VideoOnStarted;
+        video.frameReady += VideoOnFrameReady;
+        video.Prepare();
+        video.Play();
+    }
+
+    void VideoOnStarted(VideoPlayer source)
+    {
+        Debug.Log("ON STARTED");
+        source.Pause();
+    }
+
+    void VideoOnFrameReady(VideoPlayer source, long frameidx)
+    {
+        Debug.Log("ON FRAME READY");
+        Next();
+    }
+
+    // void Update()
+    // {
+    //     if (isPlayable) Next();
+    // }
+
+    /// <summary>
+    /// Show next frame.
+    /// </summary>
+    /// <returns>Return true when another frame exists.</returns>
+    public bool Next()
+    {
+        video.frame = frame++;
+        return frame <= frameCount;
+    }
+}
---
+++ Assets/Scripts/VideoRecorder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Recorder;
+using UnityEditor.Recorder.Input;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoRecorder : MonoBehaviour
+{
+    RecorderController controller;
+
+    [SerializeField]
+    VideoPlayer video;
+
+    [SerializeField, Header("Export Settings")]
+    bool isImage;
+
+    // TODO オーバーライドできるように
+    // [SerializeField]
+    // float frameRate = 30f;
+
+    [SerializeField]
+    int height = 4096;
+
+    [SerializeField]
+    int width = 4096;
+
+    [SerializeField]
+    bool renderStereo = true;
+
+    [SerializeField]
+    float stereoSeparation = 0.065f;
+
+    void Start()
+    {
+        var settings = ScriptableObject.CreateInstance<RecorderControllerSettings>();
+        settings.FrameRate = (float)video.clip.frameRate;
+        settings.CapFrameRate = true;
+        settings.FrameRatePlayback = FrameRatePlayback.Constant;
+
+        if (isImage)
+        {
+            var image = ScriptableObject.CreateInstance<ImageRecorderSettings>();
+            image.imageInputSettings = new Camera360InputSettings
+            {
+                Source = ImageSource.MainCamera,
+                MapSize = 4096,
+                OutputHeight = height,
+                OutputWidth = width,
+                RenderStereo = renderStereo,
+                StereoSeparation = stereoSeparation,
+            };
+            image.OutputFormat = ImageRecorderSettings.ImageRecorderOutputFormat.PNG;
+            image.FileNameGenerator.Root = OutputPath.Root.Absolute;
+            image.FileNameGenerator.Leaf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "image sequence");
+            image.FileNameGenerator.FileName = "image_<Frame>";
+            settings.AddRecorderSettings(image);
+        }
+        else
+        {
+            var movie = ScriptableObject.CreateInstance<MovieRecorderSettings>();
+            movie.OutputFormat = MovieRecorderSettings.VideoRecorderOutputFormat.MP4;
+            movie.ImageInputSettings = new Camera360InputSettings
+            {
+                Source = ImageSource.MainCamera,
+                MapSize = 4096,
+                OutputHeight = height,
+                OutputWidth = width,
+                RenderStereo = renderStereo,
+                StereoSeparation = stereoSeparation,
+            };
+            movie.VideoBitRateMode = VideoBitrateMode.High;
+            movie.OutputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "movie");
+            settings.AddRecorderSettings(movie);
+        }
+
+        controller = new RecorderController(settings);
+        controller.PrepareRecording();
+        controller.StartRecording();
+    }
+
+    void OnApplicationQuit()
+    {
+        controller.StopRecording();
+    }
+}
