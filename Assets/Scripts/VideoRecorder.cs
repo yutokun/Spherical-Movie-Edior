541cea0816b441ec1467478d24770e7b821efd1a
yutoVR <yuto.fushimi@icloud.com>
2020-06-09 01:35:08 +0900

    1フレームずつ連番画像に

--- Assets/Scripts/VideoRecorder.cs
+++ Assets/Scripts/VideoRecorder.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
-using UnityEditor;
 using UnityEditor.Recorder;
 using UnityEditor.Recorder.Input;
 using UnityEngine;
@@ -10,17 +10,17 @@ public class VideoRecorder : MonoBehaviour
 {
     RecorderController controller;
 
+    long frame = 1, frameCount;
+    bool nextFrameExists = true;
+
     [SerializeField]
     VideoPlayer video;
-
-    [SerializeField, Header("Export Settings")]
-    bool isImage;
 
     // TODO オーバーライドできるように
     // [SerializeField]
     // float frameRate = 30f;
 
-    [SerializeField]
+    [SerializeField, Header("Export Settings")]
     int height = 4096;
 
     [SerializeField]
@@ -32,6 +32,9 @@ public class VideoRecorder : MonoBehaviour
     [SerializeField]
     float stereoSeparation = 0.065f;
 
+    [SerializeField, Header("Debug")]
+    float interval = 0.5f;
+
     void Start()
     {
         var settings = ScriptableObject.CreateInstance<RecorderControllerSettings>();
@@ -39,49 +42,64 @@ public class VideoRecorder : MonoBehaviour
         settings.CapFrameRate = true;
         settings.FrameRatePlayback = FrameRatePlayback.Constant;
 
-        if (isImage)
+        var image = ScriptableObject.CreateInstance<ImageRecorderSettings>();
+        image.imageInputSettings = new Camera360InputSettings
         {
-            var image = ScriptableObject.CreateInstance<ImageRecorderSettings>();
-            image.imageInputSettings = new Camera360InputSettings
-            {
-                Source = ImageSource.MainCamera,
-                MapSize = 4096,
-                OutputHeight = height,
-                OutputWidth = width,
-                RenderStereo = renderStereo,
-                StereoSeparation = stereoSeparation,
-            };
-            image.OutputFormat = ImageRecorderSettings.ImageRecorderOutputFormat.PNG;
-            image.FileNameGenerator.Root = OutputPath.Root.Absolute;
-            image.FileNameGenerator.Leaf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "image sequence");
-            image.FileNameGenerator.FileName = "image_<Frame>";
-            settings.AddRecorderSettings(image);
-        }
-        else
-        {
-            var movie = ScriptableObject.CreateInstance<MovieRecorderSettings>();
-            movie.OutputFormat = MovieRecorderSettings.VideoRecorderOutputFormat.MP4;
-            movie.ImageInputSettings = new Camera360InputSettings
-            {
-                Source = ImageSource.MainCamera,
-                MapSize = 4096,
-                OutputHeight = height,
-                OutputWidth = width,
-                RenderStereo = renderStereo,
-                StereoSeparation = stereoSeparation,
-            };
-            movie.VideoBitRateMode = VideoBitrateMode.High;
-            movie.OutputFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "movie");
-            settings.AddRecorderSettings(movie);
-        }
+            Source = ImageSource.MainCamera,
+            MapSize = 4096,
+            OutputHeight = height,
+            OutputWidth = width,
+            RenderStereo = renderStereo,
+            StereoSeparation = stereoSeparation,
+        };
+        image.OutputFormat = ImageRecorderSettings.ImageRecorderOutputFormat.PNG;
+        image.FileNameGenerator.Root = OutputPath.Root.Absolute;
+        image.FileNameGenerator.Leaf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "image sequence");
+        image.FileNameGenerator.FileName = "image_<Frame>";
+        settings.AddRecorderSettings(image);
 
         controller = new RecorderController(settings);
+
+        frameCount = (long)video.frameCount;
+        video.sendFrameReadyEvents = true;
+        video.started += VideoOnStarted;
+        video.frameReady += VideoOnFrameReady;
+        video.Play();
+    }
+
+    void VideoOnStarted(VideoPlayer source)
+    {
+        source.Pause();
+    }
+
+    void VideoOnFrameReady(VideoPlayer source, long frameidx)
+    {
+        StartCoroutine(RecordFrame((int)frameidx));
+    }
+
+    IEnumerator RecordFrame(int frameId)
+    {
+        controller.Settings.SetRecordModeToSingleFrame(frameId);
+        Debug.Log($"Frame ID: {frame.ToString()}");
         controller.PrepareRecording();
         controller.StartRecording();
+        yield return new WaitForSeconds(interval);
+        controller.StopRecording();
+        if (nextFrameExists) nextFrameExists = Next();
+    }
+
+    /// <summary>
+    /// Show next frame.
+    /// </summary>
+    /// <returns>Return true when another frame exists.</returns>
+    bool Next()
+    {
+        video.frame = frame++;
+        return frame <= frameCount;
     }
 
     void OnApplicationQuit()
     {
-        controller.StopRecording();
+        controller?.StopRecording();
     }
 }
