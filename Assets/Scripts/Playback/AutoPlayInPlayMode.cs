fb19824df15414d4c6a9ab212078e8b1bef86e05
yutoVR <yuto.fushimi@icloud.com>
2020-06-14 00:06:29 +0900

    プレイモードでの自前自動再生

---
+++ Assets/Scripts/Playback/AutoPlayInPlayMode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace yutoVR.SphericalMovieEditor
+{
+    public class AutoPlayInPlayMode : MonoBehaviour
+    {
+        void Start()
+        {
+            var options = AssetDatabase.LoadAssetAtPath<RecorderOptions>(PathProvider.OptionPath);
+            if (!options) return;
+
+            if (!options.startRecordingOnEnterPlayMode)
+            {
+                GetComponent<VideoPlayer>().Play();
+            }
+        }
+    }
+}
