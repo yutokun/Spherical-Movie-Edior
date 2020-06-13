886411bd861709872d741fd5e6c8a4ddf1f8d2be
yutoVR <yuto.fushimi@icloud.com>
2020-06-13 14:06:14 +0900

    プレイモードでのヘッドセットプレビューに対応

---
+++ Assets/Scripts/Export/CameraPoseDisabler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+namespace yutoVR.SphericalMovieEditor
+{
+    public class CameraPoseDisabler : MonoBehaviour
+    {
+        void Awake()
+        {
+            var options = AssetDatabase.LoadAssetAtPath<RecorderOptions>(PathProvider.OptionPath);
+            if (!options) return;
+
+            if (options.startRecordingOnEnterPlayMode)
+            {
+                GetComponent<TrackedPoseDriver>().enabled = false;
+            }
+        }
+    }
+}
