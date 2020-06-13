1775772351de960df3f744d4ae37a5625487c033
yutoVR <yuto.fushimi@icloud.com>
2020-06-13 11:08:46 +0900

    VideoPlayerPlayableAsset がクリップの長さを認識

--- Assets/Scripts/Timeline/VideoPlayerPlayableAsset.cs
+++ Assets/Scripts/Timeline/VideoPlayerPlayableAsset.cs
@@ -8,10 +8,16 @@ public class VideoPlayerPlayableAsset : PlayableAsset
     public ExposedReference<VideoPlayer> videoPlayer;
     public Material mat;
 
+    VideoClip clip;
+
     public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
     {
         var video = videoPlayer.Resolve(graph.GetResolver());
+        clip = video.clip;
         var behaviour = new VideoPlayerPlayableBehaviour { video = video, mat = mat };
         return ScriptPlayable<VideoPlayerPlayableBehaviour>.Create(graph, behaviour);
     }
+
+    public override double duration => Duration;
+    double Duration => clip != null ? clip.length : 5;
 }
