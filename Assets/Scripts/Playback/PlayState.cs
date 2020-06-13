1a2701da1237d9a4c39dd6f38e6cc7c3faea7710
yutoVR <yuto.fushimi@icloud.com>
2020-06-14 02:24:22 +0900

    再生状態を良い感じに取得するやつ

---
+++ Assets/Scripts/Playback/PlayState.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace yutoVR.SphericalMovieEditor
+{
+    public class PlayState : MonoBehaviour
+    {
+        static PlayableDirector directorCache;
+        static PlayableDirector Director
+        {
+            get
+            {
+                if (directorCache) return directorCache;
+                var directors = FindObjectsOfType<PlayableDirector>();
+                directorCache = directors.First(d => d.GetComponent<TimelinePlayer>());
+                return directorCache;
+            }
+        }
+
+
+        public static bool IsPreviewing => Application.isPlaying && !RecorderOptions.Options.startRecordingOnEnterPlayMode;
+        public static bool IsRecording => Application.isPlaying && RecorderOptions.Options.startRecordingOnEnterPlayMode;
+        public static bool IsScrubbing => !Application.isPlaying && !Director.playableGraph.IsPlaying();
+        public static bool IsPlayingTimelineInEditor => !Application.isPlaying && Director.playableGraph.IsPlaying();
+    }
+}
